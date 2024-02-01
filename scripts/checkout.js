70854eb413c728d0f745cfccfc08695ff302beb9
jelilking <abduljelilozigi@gmail.com>
2024-02-01 13:12:50 +0100

    Delete products from Html when clicking Delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -16,7 +16,8 @@ cart.forEach((cartItem) => {
   });
 
   // console.log(matchingProduct);
-  cartSummaryHTML += ` <div class="cart-item-container">
+  cartSummaryHTML += ` <div class="cart-item-container 
+  js-cart-item-container-${matchingProduct.id}">
           <div class="delivery-date">
             Delivery date: Tuesday, June 21
           </div>
@@ -107,6 +108,12 @@ deleteLink.forEach((link) => {
   link.addEventListener("click", () => {
     const productId = link.dataset.productId;
     removeFromCart(productId);
-    console.log(cart);
+    // console.log(cart);
+
+    const container = document.querySelector(
+      `.js-cart-item-container-${productId}`
+    );
+
+    container.remove();
   });
 });
